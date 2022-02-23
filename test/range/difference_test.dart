26bed48636b7e89eb1d56800758c8188d3840a63
cyrax <bskaxe@gmail.com>
2022-02-23 14:38:57 +0700

    add tests

--- test/range/difference_test.dart
+++ test/range/difference_test.dart
@@ -72,38 +72,170 @@ void main() {
         });
       });
 
-      // test('upper bound is infinite and lower bound is finite', () {
-      //   expect(
-      //     IntRange.parse('[5,)').difference(IntRange.parse('[5,)')),
-      //     IntRange.parse('[5,)'),
-      //   );
-      //   expect(
-      //     IntRange.parse('[5,)').difference(IntRange.parse('[5,100)')),
-      //     IntRange.parse('[5,100)'),
-      //   );
-      //   expect(
-      //     IntRange.parse('[5,)').difference(IntRange.parse('empty')),
-      //     IntRange.parse('empty'),
-      //   );
-      // });
+      group('[5,) -', () {
+        test('(,)', () {
+          expect(
+            IntRange.parse('[5,)').difference(IntRange.parse('(,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('(,100)', () {
+          expect(
+            IntRange.parse('[5,)').difference(IntRange.parse('(,100)')),
+            IntRange.parse('[100,)'),
+          );
+        });
+        test('[5,)', () {
+          expect(
+            IntRange.parse('[5,)').difference(IntRange.parse('[5,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('[5,100)', () {
+          expect(
+            IntRange.parse('[5,)').difference(IntRange.parse('[5,100)')),
+            IntRange.parse('[100,)'),
+          );
+        });
+        test('empty', () {
+          expect(
+            IntRange.parse('[5,)').difference(IntRange.parse('empty')),
+            IntRange.parse('[5,)'),
+          );
+        });
+      });
 
-      // test('all bound are finite', () {
-      //   expect(
-      //     IntRange.parse('[5,100)').difference(IntRange.parse('[5,100)')),
-      //     IntRange.parse('[5,100)'),
-      //   );
-      //   expect(
-      //     IntRange.parse('[5,100)').difference(IntRange.parse('empty')),
-      //     IntRange.parse('empty'),
-      //   );
-      // });
+      group('[5,100) -', () {
+        test('(,)', () {
+          expect(
+            IntRange.parse('[5,100)').difference(IntRange.parse('(,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('(,100)', () {
+          expect(
+            IntRange.parse('[5,100)').difference(IntRange.parse('(,100)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('[5,)', () {
+          expect(
+            IntRange.parse('[5,100)').difference(IntRange.parse('[5,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('[5,100)', () {
+          expect(
+            IntRange.parse('[5,100)').difference(IntRange.parse('[5,100)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('empty', () {
+          expect(
+            IntRange.parse('[5,100)').difference(IntRange.parse('empty')),
+            IntRange.parse('[5,100)'),
+          );
+        });
+      });
 
-      // test('empty range', () {
-      //   expect(
-      //     IntRange.parse('empty').difference(IntRange.parse('empty')),
-      //     IntRange.parse('empty'),
-      //   );
-      // });
+      group('empty -', () {
+        test('(,)', () {
+          expect(
+            IntRange.parse('empty').difference(IntRange.parse('(,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('(,100)', () {
+          expect(
+            IntRange.parse('empty').difference(IntRange.parse('(,100)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('[5,)', () {
+          expect(
+            IntRange.parse('empty').difference(IntRange.parse('[5,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('[5,100)', () {
+          expect(
+            IntRange.parse('empty').difference(IntRange.parse('[5,100)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('empty', () {
+          expect(
+            IntRange.parse('empty').difference(IntRange.parse('empty')),
+            IntRange.parse('empty'),
+          );
+        });
+      });
+
+      group('[1,20) -', () {
+        test('(,)', () {
+          expect(
+            IntRange.parse('[1,20)').difference(IntRange.parse('(,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('(,100)', () {
+          expect(
+            IntRange.parse('[1,20)').difference(IntRange.parse('(,100)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('[5,)', () {
+          expect(
+            IntRange.parse('[1,20)').difference(IntRange.parse('[5,)')),
+            IntRange.parse('[1,5)'),
+          );
+        });
+        test('[5,100)', () {
+          expect(
+            IntRange.parse('[1,20)').difference(IntRange.parse('[5,100)')),
+            IntRange.parse('[1,5)'),
+          );
+        });
+        test('empty', () {
+          expect(
+            IntRange.parse('[1,20)').difference(IntRange.parse('empty')),
+            IntRange.parse('[1,20)'),
+          );
+        });
+      });
+
+      group('[80,120) -', () {
+        test('(,)', () {
+          expect(
+            IntRange.parse('[80,120)').difference(IntRange.parse('(,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('(,100)', () {
+          expect(
+            IntRange.parse('[80,120)').difference(IntRange.parse('(,100)')),
+            IntRange.parse('[100,119]'),
+          );
+        });
+        test('[5,)', () {
+          expect(
+            IntRange.parse('[80,120)').difference(IntRange.parse('[5,)')),
+            IntRange.parse('empty'),
+          );
+        });
+        test('[5,100)', () {
+          expect(
+            IntRange.parse('[80,120)').difference(IntRange.parse('[5,100)')),
+            IntRange.parse('[100,120)'),
+          );
+        });
+        test('empty', () {
+          expect(
+            IntRange.parse('[80,120)').difference(IntRange.parse('empty')),
+            IntRange.parse('[80,120)'),
+          );
+        });
+      });
     });
   });
 }
